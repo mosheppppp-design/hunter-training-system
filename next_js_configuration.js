dd2d7099f57fb8084014a1e09f1360206956394b
mosheppppp-design <mosheppppp@gmail.com>
2026-09-24 00:03:57 +0300

    Add files via upload

---
+++ next_js_configuration.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // אם תרצה לייצא את האפליקציה כסטטית בעתיד ל-GitHub Pages, תוכל להוסיף כאן output: 'export'
+};
+
+module.exports = nextConfig;
